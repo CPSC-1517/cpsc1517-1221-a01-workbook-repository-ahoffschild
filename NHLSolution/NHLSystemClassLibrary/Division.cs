ae3097d563bcceebdeb911aa8f3d109e63abbb62
Aidan <ahoffschild@outlook.com>
2023-01-11 14:51:14 -0700

    beginning of NHLclass
    arena cannot be null, city must be greater than 3 and not null, name cannot be null and must only have letters

---
+++ NHLSolution/NHLSystemClassLibrary/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLSystemClassLibrary
+{
+    internal enum Division
+    {
+        Metropolitan,
+        Atlantic,
+        Central,
+        Pacific
+    }
+}
---
+++ NHLSolution/NHLSystemClassLibrary/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NHLSystemClassLibrary
+{
+    internal class Team
+    {
+        private string Name 
+        {
+            get { return Name; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Error: empty string.");
+                }
+                else if (Regex.IsMatch, @"[a-zA-Z]+$")
+                Name = value; 
+            }
+        }
+        private string City { get; set; }
+        private string Arena { get; set; }
+        private Conference Conference { get; set; }
+        private Division Division { get; set; }
+
+        public Team(string name, string city, string arena, Conference conference, Division division)
+        {
+            Name = name;
+            City = city;
+            Arena = arena;
+            Conference = conference;
+            Division = division;
+        }
+    }
+}
