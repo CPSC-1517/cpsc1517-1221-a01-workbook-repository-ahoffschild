b5c5cb1c03b80a0561de0d8d5641982b21586b6b
Aidan <ahoffschild@outlook.com>
2023-03-13 14:47:31 -0600

    westwind setup, new project for SQL and business/design logic

---
+++ WestWindSystem/WestWindSystem/WestWindSystem/DAL/WestwindContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.DAL
+{
+    internal class WestwindContext : DbContext
+    {
+        public WestwindContext(DbContextOptions<WestwindContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BuildVersion> BuildVersions => Set<BuildVersion>();
+    }
+}
---
+++ WestWindSystem/WestWindSystem/WestWindSystem/Entities/BuildVersion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.Entities
+{
+    [Table(name: "BuildVersion:")]
+    internal class BuildVersion
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDatte { get; set; }
+    }
+}
